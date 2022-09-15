31794a99dad007fc47799b6308404b551f85f3a4
Ivan Dvurechenskiy <ivandvurechenskiy26@gmail.com>
2022-09-15 21:15:24 +0300

    Исправил аннотацию

--- src/main/java/com/example/notebooktest/api/NoteController.java
+++ src/main/java/com/example/notebooktest/api/NoteController.java
@@ -18,6 +18,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/notes")
+@CrossOrigin
 public class NoteController {
     NoteRepository noteRepository;
     NotebookRepository notebookRepository;
